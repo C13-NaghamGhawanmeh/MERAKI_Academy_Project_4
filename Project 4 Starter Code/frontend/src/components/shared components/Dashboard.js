859a33d09bc40b00c912fb01726358562543b906
NaghamGhawanmeh <ghawanmehnagham@gmail.com>
2024-12-30 18:17:43 +0300

    addComment

--- Project 4 Starter Code/frontend/src/components/shared components/Dashboard.js
+++ Project 4 Starter Code/frontend/src/components/shared components/Dashboard.js
@@ -6,13 +6,16 @@ const Dashboard = () => {
   const { token, setToken, posts, setPosts } = useContext(UserContext);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [media, setMedia] = useState("")
+  const [media, setMedia] = useState("");
+  const [comment, setcomment] = useState("");
   const [isClickedToUpdate, setIsClickedToUpdate] = useState(false);
-  const postInfo = { title, description ,media };
+  const [isCommented, setIsCommented] = useState(false);
+  const postInfo = { title, description, media };
+  const commentInfo = { comment };
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
   const getAllPosts = () => {
-    const headers = {
-      Authorization: `Bearer ${token}`,
-    };
     axios
       .get("http://localhost:5000/posts/getAllPosts", { headers })
       .then((res) => {
@@ -54,9 +57,26 @@ const Dashboard = () => {
         console.log(err);
       });
   };
+  const addComment = (id) => {
+    setIsCommented(true)
+
+    axios
+      .post(`http://localhost:5000/posts/${id}/comments/`, commentInfo, {
+        headers,
+      })
+      .then((res) => {
+      setIsCommented(false)
+      getAllPosts()
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   useEffect(() => {
     getAllPosts();
   }, []);
+
   return (
     <div>
       {posts?.map((a, index) => {
@@ -67,6 +87,9 @@ const Dashboard = () => {
               <div>description : {a.description}</div>
               {a.media.map((p, index) => {
                 return <img src={p} />;
+              })}
+              {a.comments?.map((c,index)=>{
+                return <p>{c.comment}</p>
               })}
               <button
                 id={a._id}
@@ -104,22 +127,40 @@ const Dashboard = () => {
                       setDescription(e.target.value);
                     }}
                   ></textarea>
-                  <input placeholder="media" onChange={(e) => {
+                  <input
+                    placeholder="media"
+                    onChange={(e) => {
                       setMedia(e.target.value);
-                    }}/>
+                    }}
+                  />
 
                   <button
                     id={a._id}
                     onClick={(e) => {
                       updatePostById(e.target.id);
-                    setIsClickedToUpdate(false);
-
+                      setIsClickedToUpdate(false);
                     }}
                   >
                     click
                   </button>
                 </>
               )}
+              <input
+                placeholder="Comment"
+                onChange={(e) => {
+                  setcomment(e.target.value);
+                }}
+              />
+              <button
+                id={a._id}
+                onClick={(e) => {
+                  addComment(e.target.id);
+                  setIsCommented(true);
+                }}
+              >
+                add Comment
+              </button>
+              {/* {isCommented && <p>{comment}</p>} */}
             </div>
           </div>
         );
