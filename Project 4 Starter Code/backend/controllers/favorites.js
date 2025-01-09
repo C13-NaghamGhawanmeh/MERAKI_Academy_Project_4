c1f766a91e0461819a13767917c363d6fc67633e
NaghamGhawanmeh <ghawanmehnagham@gmail.com>
2025-01-09 03:20:49 +0300

    rendering the saved component logic & Style

--- Project 4 Starter Code/backend/controllers/favorites.js
+++ Project 4 Starter Code/backend/controllers/favorites.js
@@ -3,7 +3,7 @@ const postModel = require("../models/posts");
 
 const addToFavorite = (req, res) => {
   console.log("tttttssssstttt");
-  
+
   const favoriteItem = req.params.postId;
   const favoriteOwner = req.token.userId;
   // console.log(res);
@@ -30,48 +30,54 @@ const addToFavorite = (req, res) => {
 };
 
 const getAllFavorites = (req, res) => {
-    const userId = req.token.userId;
-    // const userName = req.token.author;
-  
-    favoriteModel
-      .find({favoriteOwner :userId})
-      .populate("favoriteItem")
-      .then((result) => {
-        res.status(200).json({
-          success: true,
-          message: "All the favorites",
-        //   userId: userId,
-          favorites: result,
-        //   userName: userName,
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          success: false,
-          message: "Server Error",
-          err: err.message,
-        });
-      });
-  };
-  const deleteFavoritItemById = (req, res) => {
+  const userId = req.token.userId;
+  // const userName = req.token.author;
 
-    const favoriteItemId = req.params.id;
-    favoriteModel
-      .findOneAndDelete({ favoriteItem : favoriteItemId })
-      .then((result) => {
-        res.status(200).json({
-          success: true,
-          message: "Item deleted ",
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          success: false,
-          message: "Server Error",
-          err: err.message,
-        });
+  favoriteModel
+    .find({ favoriteOwner: userId })
+    .populate({
+      path: "favoriteItem",
+      populate: { path: "author" },
+    })
+    .populate({
+      path: "favoriteItem",
+      populate: { path: "comments", populate: { path: "commenter" } },
+    })
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "All the favorites",
+        //   userId: userId,
+        favorites: result,
+        //   userName: userName,
       });
-  };
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server Error",
+        err: err.message,
+      });
+    });
+};
+const deleteFavoritItemById = (req, res) => {
+  const favoriteItemId = req.params.id;
+  favoriteModel
+    .findOneAndDelete({ favoriteItem: favoriteItemId })
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "Item deleted ",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server Error",
+        err: err.message,
+      });
+    });
+};
 module.exports = {
   addToFavorite,
   getAllFavorites,
--- Project 4 Starter Code/frontend/src/components/shared components/Favorites.js
+++ Project 4 Starter Code/frontend/src/components/shared components/Favorites.js
@@ -2,13 +2,25 @@ import axios from "axios";
 import React, { useContext, useState } from "react";
 import { useEffect } from "react";
 import { UserContext } from "../../App";
-import { MDBBtn, MDBCard, MDBCardBody } from "mdb-react-ui-kit";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBInputGroup,
+  MDBModal,
+  MDBModalBody,
+  MDBModalContent,
+  MDBModalDialog,
+  MDBModalFooter,
+} from "mdb-react-ui-kit";
 
 const Favorites = () => {
-  const { token, userId } = useContext(UserContext);
+  const { token } = useContext(UserContext);
   const [favoritePosts, setFavoritePosts] = useState([]);
-  const [isClickedToRemove, setIsClickedToRemove] = useState(true);
-  const [target, setTarget] = useState("");
+  const [comment, setComment] = useState("");
+  const [modal3, setModal3] = useState(false);
+  const commentInfo = { comment };
 
   const headers = {
     Authorization: `Bearer ${token}`,
@@ -44,6 +56,35 @@ const Favorites = () => {
         console.log(err);
       });
   };
+  const addComment = (id) => {
+    // setIsCommented(true);
+
+    axios
+      .post(`http://localhost:5000/posts/${id}/comments/`, commentInfo, {
+        headers,
+      })
+      .then((res) => {
+        const newComment = res.data.comment;
+        setFavoritePosts((prevPosts) =>
+            prevPosts.map((post) => {
+              if (post.favoriteItem._id === id) {
+                return {
+                  ...post,
+                  favoriteItem: {
+                    ...post.favoriteItem,
+                    comments: [...post.favoriteItem.comments, newComment],
+                  },
+                };
+              }
+              return post;
+            })
+          );
+        setComment("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   useEffect(() => {
     getAllFavorites();
   }, []);
@@ -55,33 +96,126 @@ const Favorites = () => {
           <MDBCard className="TextCard">
             <MDBCardBody className="CardBody">
               <div>
-                <div>
-                  <div>
-                    <div>
-                      <h1>{p.favoriteItem.title}</h1>
+                <div className="head">
+                  <div className="titleDesc">
+                    <div className="firstLine">
+                      <h1 className="text-black font">
+                        {p.favoriteItem.author.userName}
+                      </h1>
                       <MDBBtn
                         className="save"
                         id={p.favoriteItem._id}
                         color="warning"
                         onClick={(e) => {
-                            deleteFavoriteItem(e.target.id);
-
-                        //   if (e.target.innerText === "UNSAVE") {
-                        //     if (p.favoriteItem._id === e.target.id) {
-                        //       deleteFavoriteItem(e.target.id);
-                        //   setIsClickedToRemove(!isClickedToRemove);
-                        //     }
-                        //   } else {
-                        //   setIsClickedToRemove(!isClickedToRemove);
-                        //   }
+                          deleteFavoriteItem(e.target.id);
                           console.log(e.target.innerText);
                         }}
                       >
                         Unsave
                       </MDBBtn>
                     </div>
+                    <h2 className="text-black font">{p.favoriteItem.title}</h2>
+                    <p
+                      className="text-break"
+                      style={{ width: "900px", textAlign: "start" }}
+                    >
+                      {p.favoriteItem.description}
+                    </p>
                   </div>
-                </div>
+                  </div>
+                  {p.favoriteItem.media?.map((m, index) => {
+                    return (
+                      <MDBCol lg={4} md={12} className="mb-4 mb-lg-0">
+                        <div className="bg-image hover-overlay ripple shadow-1-strong rounded ">
+                          <img src={m} className="w-100" style={{height:"400px"}}/>
+
+                          <a
+                            style={{ cursor: "pointer" }}
+                            onClick={() => setModal3(!modal3)}
+                          >
+                            <div
+                              className="mask"
+                              style={{
+                                backgroundColor: "rgba(251, 251, 251, 0.2)",
+                              }}
+                            ></div>
+                          </a>
+                        </div>
+                      </MDBCol>
+                    );
+                  })}
+
+                  {p.favoriteItem.comments?.map((c, index) => {
+                    return (
+                      <MDBCard
+                        style={{
+                          marginTop: "15px",
+                          width: "700px",
+                          height: "max-content",
+                        }}
+                      >
+                        <p
+                          className="text-break"
+                          style={{ alignSelf: "flex-start", padding: "10px" }}
+                        >
+                          {c.commenter?.userName}: {c.comment}
+                        </p>
+                      </MDBCard>
+                    );
+                  })}
+
+                  <MDBInputGroup
+                    className="mb-3"
+                    style={{ width: "600px", marginTop: "40px" }}
+                  >
+                    <input
+                      className="form-control"
+                      placeholder="Comment as username"
+                      type="text"
+                      value={comment}
+                      onChange={(e) => {
+                        setComment(e.target.value);
+                      }}
+                    />
+                    <MDBBtn
+                      outline
+                      color="warning"
+                      id={p.favoriteItem._id}
+                      onClick={(e) => {
+                        addComment(e.target.id);
+                      }}
+                    >
+                      Comment
+                    </MDBBtn>
+                  </MDBInputGroup>
+                  <MDBModal open={modal3} setShow={setModal3}>
+                    <MDBModalDialog>
+                      <MDBModalContent>
+                        <MDBModalBody>
+                          <div
+                            className="ratio ratio-16x9"
+                            style={{ width: "1000px", height: "350px" }}
+                          >
+                            <img
+                              className="lg-image"
+                              src={p.favoriteItem.media}
+                              allowFullScreen
+                              style={{ width: "470px", height: "350px" }}
+                            ></img>
+                          </div>
+                        </MDBModalBody>
+                        <MDBModalFooter>
+                          <MDBBtn
+                            onClick={() => setModal3(!modal3)}
+                            color="secondary"
+                          >
+                            Close
+                          </MDBBtn>
+                        </MDBModalFooter>
+                      </MDBModalContent>
+                    </MDBModalDialog>
+                  </MDBModal>
+               
               </div>
             </MDBCardBody>
           </MDBCard>
